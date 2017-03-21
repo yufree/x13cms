5150766199b56d83cd46047cd66fd1c0e3fcaeb7
Yufree <yufree@live.cn>
2017-03-21 16:21:46 -0400

    format and comment the code

---
+++ R/X13CMS-package.R
@@ -0,0 +1,31 @@
+
+
+#' Global tracking of isotopic labels in untargeted metabolomics
+#' 
+#' X13CMS extends XCMS's functionality to detect isotopologue groups in
+#' untargeted LC/MS profiling data that are enriched for isotope labels derived
+#' from labeled precursors. It also performs differential labeling analysis to
+#' reveal changes in labeling patterns under different biologial conditions.
+#' 
+#' \tabular{ll}{ Package: \tab X13CMS\cr Type: \tab Package\cr Version: \tab
+#' 1.3\cr Date: \tab 2014-12-07\cr License: \tab GPL-2\cr } ~~ An overview of
+#' how to use the package, ~~ ~~ including the most important functions ~~
+#' 
+#' @name X13CMS-package
+#' @aliases X13CMS-package X13CMS
+#' @docType package
+#' @author Xiaojing Huang
+#' 
+#' Maintainer: Xiaojing Huang <huangxi@@wusm.wustl.edu>
+#' @seealso
+#' @references Huang, X et al. (2014) 'X13CMS: Global Tracking of Isotopic
+#' Labels in Untargeted Metabolomics'. Anal. Chem. 86, 1632−1639.
+#' @keywords package
+#' @examples
+#' 
+#' 
+#' 
+NULL
+
+
+
